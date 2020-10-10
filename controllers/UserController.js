c24e9881cf5646977de18c59902c784fc96828c4
David <66249515+Davids-dir@users.noreply.github.com>
2020-10-10 11:22:08 +0200

    Edit UserController file
    Added new functions to read the profile (GET) and remove de user to database (DELETE).

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -27,6 +27,19 @@ const UserController = {
             console.error (error)
             res.status (500).send ({error, message: 'Ha habido un problema a la hora de obtener los datos.'})
         }
+    },
+
+    // Funcion para dar de baja el usuario
+    async delete (req, res) {
+
+        try {
+            const rem_user =  await User.deleteOne (req.body);
+            res.status (201).send ({rem_user, message: `Tu usuario ha sido de dado de baja correctamente. Espermos volver a verte pronto ${User.name} !`})
+        } 
+        catch (error) {
+            console.error (error)
+            res.status (500).send ({error, message: 'Ha habido un problema a la hora de ejecutar la operacion.'})    
+        }
     }
 }
 
--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ const UserController = require ('../controllers/UserController')
 
 router.post ('/signup', UserController.signup);
 router.get ('/profile', UserController.profile);
-
+router.delete ('/remove', UserController.delete);
 
 
 module.exports = router;
