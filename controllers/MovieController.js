816fe822284e827adbf4cc0198b463ef547596a6
David <66249515+Davids-dir@users.noreply.github.com>
2020-10-11 13:38:07 +0200

    Edit MovieController.js and movies.js
    Added functions to search movie by title and by id

--- controllers/MovieController.js
+++ controllers/MovieController.js
@@ -2,6 +2,36 @@ const {Movie} = require ('../models');
 
 const MovieController = {
 
+    // Funcion para buscar la pelicula por titulo
+    async showByTitle (req, res) {
+        try {
+            const searchTitle = await Movie.findAll ({
+                where: {title: req.body.title}
+            })
+            res.status (201). send (searchTitle);
+        } 
+        catch (error) {
+            console.error (error)
+            res.status (500).send ({error, message: 'Error en la busqueda, revisa los datos introducidos.'})
+        }
+    },
+
+    // Funcion para encontar la pelicula por el ID en la BBDD
+    async showById (req, res) {
+
+        try {
+            const searchId = await Movie.findAll ({ 
+                where: { id: req.params.id},
+                attributes: {exclude: ['createdAt', 'updatedAt']}
+            })
+            res.status (201).send(searchId);    
+        } 
+        catch (error) {
+            console.error (error)
+            res.status (500).send ({error, message: 'Hubo un problema a la hora de realizar la busqueda, revisa los datos.' })
+        }
+    },
+    
     // Funcion para obtener todo el catalogo de peliculas
     async show (req, res) {
         try {
@@ -14,7 +44,7 @@ const MovieController = {
         } 
         catch (error) {
             console.error (error)
-            res.status (500).send ({error, message: 'Hubo un problema a la hora de obtener la lista, intentalo mas tarde.'})
+            res.status (500).send ({error, message: 'Hubo un problema a la hora de realizar  la busqueda, intentalo mas tarde.'})
         }
     }
 }
--- routes/movies.js
+++ routes/movies.js
@@ -2,5 +2,7 @@ const router = require ('express').Router ();
 const MovieController = require ('../controllers/MovieController')
 
 router.get ('/list', MovieController.show);
+router.get ('/listById/:id', MovieController.showById);
+router.post ('/listByTitle', MovieController.showByTitle);
 
 module.exports = router;
